8b9abe90a4ec32d735d8c0a0b5f87377d8eedf23
The2ndSlimShady <64863810+The2ndSlimShady@users.noreply.github.com>
2024-07-12 18:50:09 +0300

    Turns out that using Debug to add debug.cpp is a bad idea. Don't ask me why.

--- build.ps1
+++ build.ps1
@@ -1,2 +1,2 @@
-cmake -S . -B build -G Ninja -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B build -G Ninja -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=DebugTest
 cmake --build build --config Debug
