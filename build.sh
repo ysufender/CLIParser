369d667554b5345d3d46c43b31facbee3ef33ab4
The2ndSlimShady <64863810+The2ndSlimShady@users.noreply.github.com>
2024-07-28 23:56:48 +0300

    forgot the shebangs

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 cmake -S . -B build -G Ninja -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=DebugTest
 cmake --build build --config Debug
--- publish.sh
+++ publish.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 cmake -S . -B build -G Ninja -DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release
 cmake --build build --config Release
